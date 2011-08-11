cfa6cac218dd4aff0bcafc55b79401a2d32eb5f4
chenzhi <chenzhi13132601@gmail.com>
2011-08-11 22:30:27 +0800

    无用提交

--- enemy.cpp
+++ enemy.cpp
@@ -31,6 +31,7 @@ m_pHeadEnity(NULL),m_HurtTime(0.0f),m_Trans(0.0f,0.0f,0.0f),m_AniFade(0.0f)
 	if(headMesh.empty()==false)
 	{
 		m_pHeadEnity=m_pSceneMrg->createEntity(headMesh);
+        assert(m_pHeadEnity);
 		m_pNode->attachObject(m_pHeadEnity);
 		m_pHeadEnity->shareSkeletonInstanceWith(m_pEntity);
 	}
