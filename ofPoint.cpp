8805acb697c81ba64b78ff5af3c433387d041055
chenzhi <chenzhi@aee8fb6d-90e1-2f42-9ce8-abdac5710353>
2011-08-23 07:30:30 +0000

    加入模式1未完成。
    git-svn-id: https://pc-cz:443/svn/GameraGame/trunk@174 aee8fb6d-90e1-2f42-9ce8-abdac5710353

--- ofPoint.cpp
+++ ofPoint.cpp
@@ -9,16 +9,4 @@
 
 #include "pch.h"
 #include "ofPoint.h"
-=======
-/*
- *  ofPoint.cpp
- *  Accelerometer
- *
- *  Created by chen zhi on 11-6-18.
- *  Copyright 2011 sx. All rights reserved.
- *
- */
 
-#include "pch.h"
-#include "ofPoint.h"
->>>>>>> ae44b686e40af873961f9f67adef8c05ce8182d0
